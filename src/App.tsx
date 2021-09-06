d40f684e2bac291de303a0b063fe7078a5f67168
20310381 <20310381@qq.com>
2021-09-06 19:33:47 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-2xl text-red-700">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-9xl"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+
         >
           Learn React
         </a>
