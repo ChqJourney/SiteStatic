b99564a65cbf8b5db275dd7ab652ba35babbcd0b
patrick <patrickchq@outlook.com>
2021-09-08 23:16:36 +0800

    css study

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,29 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import { Head } from './layouts/head';
 
 function App() {
   return (
-    <>
-      <header className="w-full sticky top-0 bg-white bg-opacity-80 flex justify-between items-center py-3" >
-        {/* big icon */}
-        <div className="font-bold mx-9 "><a href="#" className="cursor-pointer text-3xl hover:text-indigo-400">Photone<span className="text-indigo-500 text-5xl font-serif italic">e</span></a></div>
-        {/* desktop nav area */}
-        <div className="md:flex justify-around items-center hidden">
+    <Switch>
+      <Route path="/home">
+        <div>home</div>
+      </Route>
+      <Route path="/">
+        <Head/>
+        <div className="container px-5 mx-auto flex flex-wrap">
+          <div className="w-full md:w-2/3 flex flex-col">
+            <div className="text-5xl font-bold my-2.5 text-center md:text-left">We are making thing simpler...build best tools for you.</div>
+            <div className="text-1xl font-light text-center md:text-left my-4">nine4 is a free to use website template for websites made with Next.js and styled with Tailwind CSS</div>
+            <button className="abtn-black">Find out more</button>
+          </div>
+          <div className="w-full md:w-1/3 flex flex-col items-center">
+            <img alt="" width="200" src="https://nine4-2.vercel.app/images/iPhone-12-Mockup.png" />
+          </div>
 
-          <a href="#" className="font-sans font-semibold  mx-4 no-underline hover:text-indigo-400 hover:underline">Home</a>
-          <a href="#" className="font-sans font-semibold mx-4 no-underline hover:text-indigo-400 hover:underline">App</a>
-          <a href="#" className="font-sans font-semibold mx-4 no-underline hover:text-indigo-400 hover:underline">About us</a>
-
-          <a href="#" className="px-4 py-2
-    text-sm font-semibold text-center
-    text-white transition
-    duration-200 ease-in-out
-   bg-gray-900 rounded-md
-    cursor-pointer
-    hover:bg-gray-600 mx-4">Login <i className="fa fa-arrow-right" aria-hidden="true"></i></a>
-        </div>
-        {/* mobile nav area */}
-        <div className="btn cursor-pointer text-3xl mx-4 flex justify-center items-center md:hidden">
-          <i className="fa fa-bars" aria-hidden="true"></i>
-        </div>
-      </header>
-      <div className="container mx-auto flex flex-wrap">
-        <div className="w-full md:w-2/3 flex flex-col">
-          <div className="text-5xl font-bold my-2.5 text-center md:text-left">We are making Stunning Websites</div>
-          <div className="text-1xl font-light text-center md:text-left my-4">nine4 is a free to use website template for websites made with Next.js and styled with Tailwind CSS</div>
-          <a className="capitalize px-2 py-2 md:text-left text-white btn transition
-    duration-200 ease-in-out
-   bg-gray-900 rounded-md
-    cursor-pointer
-    hover:bg-gray-600 w-36 mx-auto md:mx-1.5">Find out more</a>
-        </div>
-        <div className="w-full md:w-1/3 flex flex-col items-center">
-          <img alt="" width="200" src="https://nine4-2.vercel.app/images/iPhone-12-Mockup.png" />
         </div>
 
-      </div>
-      <div className="container mx-auto flex flex-wrap">
-        <div className="w-full md:w-2/3 flex flex-col">
-          <div className="text-5xl font-bold my-2.5 text-center md:text-left">We are making Stunning Websites</div>
-          <div className="text-1xl font-light text-center md:text-left my-4">nine4 is a free to use website template for websites made with Next.js and styled with Tailwind CSS</div>
-          <a className="capitalize px-2 py-2 md:text-left text-white btn transition
-    duration-200 ease-in-out
-   bg-gray-900 rounded-md
-    cursor-pointer
-    hover:bg-gray-600 w-36 mx-auto md:mx-1.5">Find out more</a>
-        </div>
-        <div className="w-full md:w-1/3 flex flex-col items-center">
-          <img width="200" src="https://nine4-2.vercel.app/images/iPhone-12-Mockup.png" />
-        </div>
-
-      </div>
-    </>
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/layouts/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export const Main=()=>{
+    return (
+        <div className="container px-5 mx-auto flex flex-wrap">
+            
+        </div>
+    )
+}
