a7dcae099c2d94897f97cc8cea2d6d8224274bf1
patrick <patrickchq@outlook.com>
2021-09-20 13:55:22 +0800

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import { Route, Switch,Redirect } from 'react-router-dom';
 import { Head } from './layouts/head';
+import { Main } from './layouts/main';
+import { Landing } from './pages/landing';
+import { Mobile } from './pages/mobile';
+import { Test } from './pages/test';
 
 function App() {
   return (
     <Switch>
       <Route path="/home">
-        <div>home</div>
+        <Mobile/>
       </Route>
       <Route path="/">
         <Head/>
-        <div className="container px-5 mx-auto flex flex-wrap">
-          <div className="w-full md:w-2/3 flex flex-col">
-            <div className="text-5xl font-bold my-2.5 text-center md:text-left">We are making thing simpler...build best tools for you.</div>
-            <div className="text-1xl font-light text-center md:text-left my-4">nine4 is a free to use website template for websites made with Next.js and styled with Tailwind CSS</div>
-            <button className="abtn-black">Find out more</button>
-          </div>
-          <div className="w-full md:w-1/3 flex flex-col items-center">
-            <img alt="" width="200" src="https://nine4-2.vercel.app/images/iPhone-12-Mockup.png" />
-          </div>
-
-        </div>
+        <Main>
+          <Route path="/test">
+            <Test/>
+          </Route>
+          <Route path="/landing">
+            <Landing/>
+          </Route>
+          <Route path="/">
+            <Redirect to="/landing"/>
+          </Route>
+        </Main>
 
       </Route>
     </Switch>
--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 
-export const Main=()=>{
+export const Main:React.FC=({children})=>{
     return (
         <div className="container px-5 mx-auto flex flex-wrap">
-            
+            {children}
         </div>
     )
 }
