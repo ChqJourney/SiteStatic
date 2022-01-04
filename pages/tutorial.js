b59337d38f857d3e99616bfed5d2d4765f367fc8
patrick <patrickchq@outlook.com>
2022-01-04 22:51:25 +0800

    routine

--- pages/tutorial.js
+++ pages/tutorial.js
@@ -1,60 +1,89 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export default function Tutorial({ message, data }) {
-  //   let clientIp = "";
-  //   let clientInfo = "";
-  //   useEffect(() => {
-  //     fetchIpInfo();
-  //     async function fetchIpInfo() {
-  //       const res = await fetch("http://122.51.114.20:5555/ipwithaddr");
-  //       console.log(res)
-  //       const body = await res.json();
-  //       clientIp = body.data.ipAddress;
-  //       clientInfo = body.data.regions;
-  //     }
-  //   }, []);
+async function fetchIpInfo(ip) {
+    if(!f_check_IP(ip)){
+        alert("not a IP address with correct format")
+        return
+    }
+    const res = await fetch(`http://122.51.114.20:5555/ipsearchwithaddr?ipaddr=${ip}`);
+    const body = await res.json();
+    console.log(body)
+    return body.data
+  }
+function f_check_IP(ip)    
+{  
+   var re=/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/;//正则表达式   
+   if(re.test(ip))   
+   {   
+       if( RegExp.$1<256 && RegExp.$2<256 && RegExp.$3<256 && RegExp.$4<256) 
+       return true;   
+   }   
+   
+   return false;    
+}
+
+export default function Tutorial({message,data}) {
+    const [searchIp,setSearchIp]=useState("")
+    const [seartchIpInfo,setSearchIpInfo]=useState({ipAddress:"",regions:""})
+    const [regionInfo,setRegionInfo]=useState({})
+    function searchIpAddr(e,ip){
+        e.preventDefault()
+        fetchIpInfo(ip).then(res=>{
+            if(res){
+                const regions=res.regions.split("，")
+                setRegionInfo({country:regions[0],province:regions[1],city:regions[2],provider:regions[3]})
+                
+            }
+        })
+    }
+    
+    
   return (
-    <div className="container py-10 mx-auto justify-center grid grid-cols-3 gap-4">
-      <div className="flex text-center col-span-3 justify-center">
+    <div className="container  py-10 mx-auto justify-center grid grid-cols-4 gap-4">
+      <div className="flex text-center col-span-4 justify-center">
         <p className="text-center text-xl">您的IP地址为</p>
         <div className=" text-xl text-red-300">{data.ipAddress}</div>
         <div className=" text-xl">，</div>
       </div>
-      <div className="flex text-center col-span-3 justify-center">
+      <div className="flex text-center col-span-4 justify-center">
         <p className="text-xl">您位于</p>
         <div className=" text-sky-800 text-xl">{data.regions}</div>
         <div className=" text-xl">！</div>
       </div>
-      <div className="flex justify-center col-start-2">
+      <div className="flex items-center justify-center col-start-2 col-span-2">
         <input
           type="text"
           name="ipAddress"
-          id="ipAddress"
-          className="mt-1 h-8 text-2xl focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-500 px-1 border-2 rounded-md"
+          id="ipAddress" onChange={(e)=>setSearchIp(e.target.value)}
+          className="mt-1 h-8 select-auto text-center text-2xl focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-500 px-1 border-2 rounded-md"
         />
-        <button className="mx-2 w-36 btn-primary">查询</button>
+        <button className="mx-2 w-36 h-8 btn-color" onClick={(e)=>searchIpAddr(e,searchIp)}>查询</button>
       </div>
-      <div className=" col-span-3 flex justify-center">
+      <div className=" col-span-2 col-start-2 flex justify-center">
 
-      <table className=" w-6/12 border-collapse border border-gray-500 ...">
-        <thead>
+      <table className=" w-full border-collapse border border-gray-500">
+        <thead className="bg-gray-50">
           <tr>
-            <th className="border border-gray-600 ...">State</th>
-            <th className="border border-gray-600 ...">City</th>
+            <th className="border border-gray-600 w-4/12">信息</th>
+            <th className="border border-gray-600 text-center">详情</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="border border-gray-700 ...">Indiana</td>
-            <td className="border border-gray-700 ...">Indianapolis</td>
+          <tr className="bg-gray-300">
+            <td className="border border-gray-700 w-6/12 text-center">国家</td>
+            <td className="border border-gray-700 text-center">{regionInfo.country??""}</td>
           </tr>
-          <tr>
-            <td className="border border-gray-700 ...">Ohio</td>
-            <td className="border border-gray-700 ...">Columbus</td>
+          <tr className="bg-gray-300">
+            <td className="border border-gray-700 w-6/12 text-center">省份</td>
+            <td className="border border-gray-700 text-center">{regionInfo.province}</td>
           </tr>
-          <tr>
-            <td className="border border-gray-700 ...">Michigan</td>
-            <td className="border border-gray-700 ...">Detroit</td>
+          <tr className="bg-gray-300">
+            <td className="border border-gray-700 w-6/12 text-center">城市</td>
+            <td className="border border-gray-700 text-center">{regionInfo.city}</td>
+          </tr>
+          <tr className="bg-gray-300">
+            <td className="border border-gray-700 w-6/12 text-center">运营商</td>
+            <td className="border border-gray-700 text-center">{regionInfo.provider}</td>
           </tr>
         </tbody>
       </table>
@@ -66,7 +95,6 @@ export default function Tutorial({ message, data }) {
 export async function getServerSideProps(context) {
   const res = await fetch("http://122.51.114.20:5555/ipwithaddr");
   const body = await res.json();
-  console.log(body);
   return {
     props: body,
   };
