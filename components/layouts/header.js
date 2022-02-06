f5a694976d6af1b46a50907bfe59d4ead554dc83
patrick <patrickchq@outlook.com>
2022-02-07 00:17:45 +0800

    routine

--- components/layouts/header.js
+++ components/layouts/header.js
@@ -177,7 +177,7 @@ function Header({ menus }) {
                         <Menu.Item>
                           {({ active }) => (
                             <a
-                              href="/api/auth/me"
+                              href="#"
                               className={classNames(
                                 active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700"
