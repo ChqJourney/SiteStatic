a4d5317bb6b308ae6e28e01756fbbe67f6440793
patrick <patrickchq@outlook.com>
2022-02-11 23:46:15 +0800

    routine

--- components/layouts/contentCard.js
+++ components/layouts/contentCard.js
@@ -8,13 +8,16 @@ export const ContentCard=({picEle,title,description,href})=>{
 
    
     return (
-        <div className='max-w-7xl lg:col-span-2 xl:col-span-4 flex items-center px-4 w-full border rounded-lg'>
+        <div className='max-w-7xl col-span-4 md:col-span-2 text-center xl:col-span-4 static xl:flex items-center px-4 w-full border rounded-lg'>
+           <div className='flex justify-center mt-4'>
+
            {picEle}
+           </div>
             <div className='flex flex-col justify-center ml-8 h-48'>
                 <div className='font-medium text-2xl'>{title}</div>
                 <div className='text-lg my-2 text-gray-400  overflow-x-hidden'>{description}</div>
             </div>
-            <button className='btn-color ml-4' ><Link href={href} passHref>
+            <button className='btn-color ml-4 mb-4' ><Link href={href} passHref>
             <a >Go try</a>
             </Link>
             </button>
