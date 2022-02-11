0cece909185a7b3a6547be29465599d5c947a873
patrick <patrickchq@outlook.com>
2022-02-12 00:09:40 +0800

    routine

--- components/layouts/contentCard.js
+++ components/layouts/contentCard.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 export const ContentCard=({picEle,title,description,href})=>{
 
-   
+   console.log('ddd')
     return (
         <div className='max-w-7xl col-span-4 md:col-span-2 text-center xl:col-span-4 static xl:flex items-center px-4 w-full border rounded-lg'>
            <div className='flex justify-center mt-4'>
