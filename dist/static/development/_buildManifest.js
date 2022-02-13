088f05df94ebe4b62f3cbca5a2220520b8c76915
patrick <patrickchq@outlook.com>
2022-02-14 00:27:23 +0800

    update gitignore

--- dist/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/images/idcut":["static\u002Fchunks\u002Fpages\u002Fimages\u002Fidcut.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fimages\u002Fidcut"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
