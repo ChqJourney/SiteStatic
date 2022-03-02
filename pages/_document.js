38a53f3c0fb3e284b160ee12096402d5ca52ddfd
ChqJourney <45679161+ChqJourney@users.noreply.github.com>
2022-03-02 19:34:37 +0800

    add google analysis

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
+  
 
   render() {
     return (
@@ -14,7 +15,17 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
           />
-         
+         <script async src="https://www.googletagmanager.com/gtag/js?id=G-TKD4BGQZ37"></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', G-TKD4BGQZ37);`
+            }}
+          />
         </Head>
         <body className='font-sans antialiased text-gray-600'>
           <Main />
